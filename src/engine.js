fe194f8dfd09e9fd872fbe19784de411f31197c1
KOT <graffdekot027@gmail.com>
2026-01-20 03:21:24 +0300

    modified

--- src/engine.js
+++ src/engine.js
@@ -29,4 +29,4 @@ const getRandomNumber = (min = 1, max = 100) => {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
 
-export { runGame, getRandomNumber}
+export { runGame, getRandomNumber }
